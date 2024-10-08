35ea64634bafd71a227a67772200b3b5591a5613
muba123321/ <laarychambas@gmail.com>
2024-10-07 23:34:31 -0400

    created a new project on MongoDb And connected to the mongodb with mongoose

---
+++ activityHub-BackEnd/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("DB connected successfully");
+  } catch (err) {
+    console.log("Error connecting to DB", err.message);
+    process.exit(1);
+  }
+};
+
+export default dbConnection;
--- activityHub-BackEnd/index.js
+++ activityHub-BackEnd/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import dbConnection from "./config/db.js";
 
 const app = express();
+dbConnection();
 const port = 3000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// activityCapstone
+// chambas
